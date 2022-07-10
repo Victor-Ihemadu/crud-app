596f7336dab3a38d184167324f32dfd34899baf5
Victor-Ihemadu <victorihemadu@gmail.com>
2022-07-10 19:07:14 +0100

    new update

--- main.go
+++ main.go
@@ -1,1 +1,41 @@
 package main
+
+import(
+	"fmt"
+	"log"
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"strconc"
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json: "director"`
+
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	router := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "40939", Title: "Spiderman", Director: &Director{Firstname: "Victor", Lastname: "Ihemadu"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "74903", Title: "StrangerThings", Director: &Director{Firstname: "Peace", Lastname: "Onyinye"}})
+	router.HandleFunc("/movies", getMovies).Methods("GET")
+	router.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	router.HandleFunc("/movies", createMovie).Methods("POST")
+	router.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	router.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
